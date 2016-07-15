ac0d1f781e96580def276f00aa5399257df83bd1
BaekJiGyeong <wlrud0126@hanmail.net>
2016-07-15 09:36:22 +0900

    patientLoc

--- SSMS/src/main/java/com/ktds/ssms/article/biz/impl/ArticleBizImpl.java
+++ SSMS/src/main/java/com/ktds/ssms/article/biz/impl/ArticleBizImpl.java
@@ -1,10 +1,13 @@
 package com.ktds.ssms.article.biz.impl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ktds.ssms.article.biz.ArticleBiz;
 import com.ktds.ssms.article.dao.ArticleDAO;
+import com.ktds.ssms.member.vo.MemberVO;
 
 public class ArticleBizImpl implements ArticleBiz {
 
@@ -15,6 +18,11 @@ public class ArticleBizImpl implements ArticleBiz {
 	   public void setArticleDAO(ArticleDAO articleDAO) {
 	      this.articleDAO = articleDAO;
 	   }
+
+	@Override
+	public List<MemberVO> getPatientVOListByName(String name) {
+		return articleDAO.getPatientVOListByName(name);
+	}
 
 	  
 
