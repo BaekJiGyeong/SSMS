657bc7c1ea7b0621742b8c0ca89254d4c2783579
BaekJiGyeong <wlrud0126@hanmail.net>
2016-07-15 10:37:50 +0900

    patientLoc

---
+++ SSMS/src/main/java/com/ktds/ssms/article/vo/LocationVO.java
@@ -0,0 +1,44 @@
+package com.ktds.ssms.article.vo;
+
+public class LocationVO {
+	
+	private String locationId;
+	private String time;
+	private String beaconId;
+	private String marginLeft;
+	private String marginTop;
+	
+	public String getLocationId() {
+		return locationId;
+	}
+	public void setLocationId(String locationId) {
+		this.locationId = locationId;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getBeaconId() {
+		return beaconId;
+	}
+	public void setBeaconId(String beaconId) {
+		this.beaconId = beaconId;
+	}
+	public String getMarginLeft() {
+		return marginLeft;
+	}
+	public void setMarginLeft(String marginLeft) {
+		this.marginLeft = marginLeft;
+	}
+	public String getMarginTop() {
+		return marginTop;
+	}
+	public void setMarginTop(String marginTop) {
+		this.marginTop = marginTop;
+	}
+	
+	
+
+}
